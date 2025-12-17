c67b590daee0526a0e06deb3b52b5604f30accce
JathanNevons <756048@newcollege.ac.uk>
2025-12-17 18:27:16 +0000

    Finished

--- Assets/BasicCoffee.cs
+++ Assets/BasicCoffee.cs
@@ -24,18 +24,22 @@ public class BasicCoffee : MonoBehaviour, IInteractable
     public void Interact()
     {
         if (!CanInteract()) return;
+        OpenBasicCoffee();
     }
     private void OpenBasicCoffee()
     {
-        //SetOpened
-
+        SetOpened(true);
         //DropItem
+        if (itemprefab)
+        {
+            GameObject droppedItem = Instantiate(itemprefab, transform.position + Vector3.down, Quaternion.identity);
+        }
     }
+    
 
     public void SetOpened(bool opened)
     {
-        IsOpened = opened;
-        if (IsOpened)
+        if (IsOpened = opened)
         {
             GetComponent<SpriteRenderer>().sprite = openedSprite;
         }
